23211cc9458c481cd138df1da6f0443a3787463e
mmhw <90556466+mmhw@users.noreply.github.com>
2021-11-22 16:27:21 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+
+
+SIZE = 1024
+PORT = 12345
+
+my_socket = socket.socket()
+my_socket.bind(("", PORT))
+my_socket.listen()
+
+print("Welcome to FTP server")
+
+while True:
+    client_socket, address = my_socket.accept()
+    print(f'{address} is connected')
+    
+    command = client_socket.recv(SIZE).decode()
+    file_name = client_socket.recv(SIZE).decode().split('/')[-1]
+    
+    if command == 'upload':
+        with open(f'mefathim4/socket/server/{file_name}', 'ab+') as f:
+            data = client_socket.recv(SIZE)
+            
+            f.write(data)                
+
+    elif command == 'download':
+        pass
+
+    elif command == 'list':
+        pass
+
+    else:
+        pass
