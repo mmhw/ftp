77eb8a7b3440d7a26acedfec59f27af9225112c0
mmhw <90556466+mmhw@users.noreply.github.com>
2021-11-23 12:05:20 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import os
 
 SIZE = 1024
 PORT = 12345
@@ -7,32 +7,64 @@ PORT = 12345
 my_socket = socket.socket()
 my_socket.bind(("", PORT))
 my_socket.listen()
+print("[+] Welcome to FTP server\n")
 
-print("Welcome to FTP server")
 
-while True:
-    client_socket, address = my_socket.accept()
-    print(f'{address} is connected')
-    
+def cli():
     command = client_socket.recv(SIZE).decode()
-    file_name = client_socket.recv(SIZE).decode().split('/')[-1]
     
     if command == 'upload':
-        with open(f'mefathim4/socket/server/{file_name}', 'ab+') as f:
-            data = client_socket.recv(SIZE)
-            
-            f.write(data)                
+        upload()
 
     elif command == 'download':
-        with open(file_name, 'rb') as f:
-            data = f.read(SIZE)
-
-            while data:
-                client_socket.send(data)
-                data = f.read(SIZE)
+        download()
 
     elif command == 'list':
-        pass
-
+        list_files()
+    
     else:
         pass
+
+
+def upload():
+    file_name = client_socket.recv(SIZE).decode().split('/')[-1]
+    print(f"[+] [received] upload, file name: {file_name}")
+
+    with open(f'/home/david/mefathim4/socket/server/{file_name}', 'wb+') as f:
+        data = client_socket.recv(SIZE)
+
+        while data:
+            f.write(data)
+            data = client_socket.recv(SIZE)
+    
+    print("[!] Done")
+
+
+def download():
+    file_name = client_socket.recv(SIZE).decode().split('/')[-1]
+    print(f"[+] [received] download, file name: {file_name}")
+
+    with open(f'/home/david/mefathim4/socket/server/{file_name}', 'rb') as f:
+        data = f.read(SIZE)
+
+        client_socket.send(data)                          
+    
+    print("[!] Done")
+
+
+def list_files():
+    print(f"[+] [received] list all files")
+    
+    all_files = os.listdir('/home/david/mefathim4/socket/server/')        
+    
+    client_socket.send(str(all_files).encode())
+
+    print("[!] Done")
+
+
+if __name__=="__main__":
+    while True:
+        client_socket, address = my_socket.accept()               
+        print(f'[!] {address} is connected')
+        
+        cli()
